9023e6015dfb231480419286a9ce3e7996ccde93
DeeprajDeveloper <deepraj.developer@gmail.com>
2025-03-18 19:49:15 -0400

    INITIAL - project structure

---
+++ app/constants.py
@@ -0,0 +1,8 @@
+class Information:
+    VERSION: str = 'v.1.0.0.20250210'
+    AUTHOR: str = 'Adhikary, Deepraj'
+    CREATE_DATE: str = 'Mar 18, 2025'
+    RELEASE_DATE: str = 'Mar 18, 2025'
+    JOB_START_YEAR: int = 2018
+    JOB_START_MONTH_ID: int = 6
+
