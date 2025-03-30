2bb23dcb7de0760677d7096046ed1ecd3012aee0
DeeprajDeveloper <deepraj.developer@gmail.com>
2025-03-29 21:59:13 -0400

    Commit - 032925 - Static Home Page Design

--- app/gui/static/scripts/uiScripts.js
+++ app/gui/static/scripts/uiScripts.js
@@ -6,10 +6,22 @@ var projectMenu = document.getElementById("project");
 var contactMenu = document.getElementById("contact");
 
 burgerIcon.addEventListener('click', () => {
+    
     if(burgerMenu.style.display === '' || burgerMenu.style.display === 'none') {
         burgerMenu.style.display = 'block';
+        burgerIcon.classList.add("burger_icon_clicked");
     } else {
         burgerMenu.style.display = "none";
+        burgerIcon.classList.remove("burger_icon_clicked");
+    }
+});
+
+
+document.addEventListener('click', (event) => {
+    
+    if (!burgerMenu.contains(event.target) && !burgerIcon.contains(event.target)) {
+        burgerMenu.style.display = "none";
+        burgerIcon.classList.remove("burger_icon_clicked");
     }
 });
 
--- app/gui/views.py
+++ app/gui/views.py
@@ -1,6 +1,7 @@
 import os
 from flask import Blueprint, jsonify, request, render_template
 from app.gui import functionality as func
+from app.constants import Information as const
 
 bp_gui = Blueprint('bp_gui', __name__, url_prefix='/gui', template_folder='templates', static_folder=os.path.join(os.path.dirname(__file__), 'static'), static_url_path='/bp_gui/static')
 
@@ -8,3 +9,14 @@ bp_gui = Blueprint('bp_gui', __name__, url_prefix='/gui', template_folder='templ
 @bp_gui.route('/test', methods=['GET'])
 def test():
     return render_template('test.html')
+
+
+@bp_gui.route('/admin', methods=['GET'])
+def admin():
+    return render_template('admin.html')
+
+
+@bp_gui.route('/', methods=['GET'])
+@bp_gui.route('/home', methods=['GET'])
+def home():
+    return render_template('index.html', author=const.AUTHOR, version=const.VERSION)
